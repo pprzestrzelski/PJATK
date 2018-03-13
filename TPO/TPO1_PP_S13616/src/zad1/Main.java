e96b566f50adfcabe31bfb3ef33be6b006842b9d
pprzestrzelski <pawel.przestrzelski@gmail.com>
2018-03-13 20:08:38 +0100

    Init commit for TPO1

---
+++ TPO/TPO1_PP_S13616/src/zad1/Main.java
@@ -0,0 +1,16 @@
+/**
+ *
+ *  @author Przestrzelski Paweł S13616
+ *
+ */
+
+package zad1;
+
+
+public class Main {
+  public static void main(String[] args) {
+    String dirName = System.getProperty("user.home")+"/TPO1dir";
+    String resultFileName = "TPO1res.txt";
+    Futil.processDir(dirName, resultFileName);
+  }
+}
