cda4914eb8f7f5cd0056de89fea504461a25021e
pprzestrzelski <pawel.przestrzelski@gmail.com>
2018-04-16 21:46:12 +0200

    Implement missing functionalities

--- TPO/TPO2_PP_S13616/src/zad1/NetClientGui.java
+++ TPO/TPO2_PP_S13616/src/zad1/NetClientGui.java
@@ -5,38 +5,41 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.SimpleDateFormat;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Date;
-import java.util.Locale;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-import org.json.JSONObject;
-
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class NetClientGui extends JFrame 
 {
+	private static final int MAX_CURRENCY_LENGTH = 3;
 	private static final long serialVersionUID = -4802716159375012064L;
 	private Service netService = null;
 	private JPanel topPanel, bottomPanel;
 	private JFXPanel middlePanel;
-	private JTextField countryTextField, cityTextField;
-	private JLabel countryLabel, cityLabel;
-	
+	private JTextField countryTextField, cityTextField, foreignCurrencyTextField;
+	private JLabel countryLabel, cityLabel, foreignCurrencyLabel;
+	private JLabel weatherInfoLabel, currencyInfoLabel;
+	private final String weatherFormat, currencyFormat;
 	private WebEngine webEngine;
 	private final int FIELDS_LENGTH = 20;
-	private final String ISO8601_FORMAT = "yyyy-MM-dd HH:mm:ss";
-	JButton searchBtn = null;
+	private JButton searchBtn = null;
 
 	public NetClientGui(String name)
 	{
@@ -47,35 +50,59 @@ public class NetClientGui extends JFrame
 		setupMiddleArea();	// WebBrowser
 		setupBottomArea();	// Weather, currency et al.
 		
-		getContentPane().setPreferredSize(new Dimension(800, 665));
+		getContentPane().setPreferredSize(new Dimension(800, 600));
 		pack();
 		setVisible(true);
+		
+		weatherFormat = new String("<html>Weather information<i><br>Date: %s<br>Temperature: %.1f C<br>Pressure: %.0f hPa<br>Humidity: %.0f %%<br>" +
+		   		   				   "Wind speed: %.1f m/s<br>Wind direction: %.1f deg</i></html>");
+		currencyFormat = new String("<html>Currency rates<br><i>%s/%s: %.4f<br>" +
+		   		   					"PLN from NBP: %.4f</i></html>");
 	}
 	
 	private void setupTopArea()
 	{
 		topPanel = new JPanel();
-		topPanel.setLayout(new GridLayout(2, 3, 3, 3));
+		topPanel.setLayout(new GridLayout(2, 4, 3, 3));
 		
 		searchBtn = new JButton("Search");
 		searchBtn.addActionListener(runService);
-		topPanel.add(new JPanel());
 		
+		// Top labels
 		countryLabel = new JLabel("Country:", JLabel.LEFT);
 		cityLabel = new JLabel("City:", JLabel.LEFT);
+		foreignCurrencyLabel = new JLabel("Foreign currency to compare:", JLabel.LEFT);
+		topPanel.add(new JPanel());	// Blank space
 		topPanel.add(countryLabel);
 		topPanel.add(cityLabel);
+		topPanel.add(foreignCurrencyLabel);
 		
+		// Bottom control fields and button
 		countryTextField = new JTextField(FIELDS_LENGTH);
 		countryTextField.setText("Poland");
 		cityTextField = new JTextField(FIELDS_LENGTH);
 		cityTextField.setText("Warsaw");
+		
+		foreignCurrencyTextField = new JTextField(FIELDS_LENGTH);
+		foreignCurrencyTextField.setText("USD");
+		// Limit a text filed with foreign currency to MAX_CURRENCY_LENGTH
+		foreignCurrencyTextField.addKeyListener(new KeyAdapter() 
+		{
+		    public void keyTyped(KeyEvent e) 
+		    { 
+		        if (foreignCurrencyTextField.getText().length() >= MAX_CURRENCY_LENGTH)
+		            e.consume(); 
+		    }  
+		});
+		
 		topPanel.add(searchBtn);
 		topPanel.add(countryTextField);
 		topPanel.add(cityTextField);
+		topPanel.add(foreignCurrencyTextField);
 		
 		add(topPanel, BorderLayout.PAGE_START);
 	}
+	
 	
 	private void setupMiddleArea()
 	{
@@ -92,22 +119,42 @@ public class NetClientGui extends JFrame
 		});
 	}
 	
+	// https://stackoverflow.com/questions/20413935/make-javafx-scene-stretch-with-container-frame
 	private void initWebEngine()
 	{
-		Group group = new Group();
-		Scene scene = new Scene(group);
+		AnchorPane anchorPane = new AnchorPane();
+		WebView webView = new WebView();
+
+		//Set Layout Constraint
+		AnchorPane.setTopAnchor(webView, 0.0);
+		AnchorPane.setBottomAnchor(webView, 0.0);
+		AnchorPane.setLeftAnchor(webView, 0.0);
+		AnchorPane.setRightAnchor(webView, 0.0);
+
+		anchorPane.getChildren().add(webView);
+		final Scene scene = new Scene(anchorPane);
 		middlePanel.setScene(scene);
 
-		WebView browser = new WebView();
-		group.getChildren().add(browser);
-		
-		webEngine = browser.getEngine();
+		webEngine = webView.getEngine();
+		// Load default webpage:
 		//webEngine.load("http://www.google.com");
 	}
 	
 	private void setupBottomArea()
 	{
 		bottomPanel = new JPanel();
+		bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.LINE_AXIS));
+		
+		weatherInfoLabel = new JLabel();
+		weatherInfoLabel.setHorizontalAlignment(JLabel.CENTER);
+		weatherInfoLabel.setVerticalAlignment(JLabel.TOP);
+		
+		currencyInfoLabel = new JLabel();
+		currencyInfoLabel.setHorizontalAlignment(JLabel.CENTER);
+		currencyInfoLabel.setVerticalAlignment(JLabel.TOP);
+		
+		bottomPanel.add(weatherInfoLabel);
+		bottomPanel.add(currencyInfoLabel);
 		add(bottomPanel, BorderLayout.PAGE_END);
 	}
 	
@@ -121,21 +168,8 @@ public class NetClientGui extends JFrame
 			netService = new Service(country);
 			String weatherJson = netService.getWeather(city);
 			
-			// https://openweathermap.org/current
-			//viewBasicWeatherParameters(weatherJson);
-			JSONObject json = new JSONObject(weatherJson);
-			double dt = json.getDouble("dt");	// dt - time in ms since January 1st, 1970 GMT
-			Date weatherDate = new Date((long) dt * 1000);
-			double temp = json.getJSONObject("main").getDouble("temp");
-			double pressure = json.getJSONObject("main").getDouble("pressure");
-			double humidity = json.getJSONObject("main").getDouble("humidity");
-			double windSpeed = json.getJSONObject("wind").getDouble("speed");
-			double windDir = json.getJSONObject("wind").getDouble("deg");
-				
-			//System.out.println(weatherDate + " " + temp + " " + pressure + " " 
-			//					 + humidity + " " + windSpeed + " " + windDir);
-			//System.out.println(netService.getRateFor("USD"));
-			//System.out.println(weatherJson);
+			showWeatherParameters(weatherJson);
+			showCurrencyRates();
 						
 			Platform.runLater(new Runnable()
 			{
@@ -147,4 +181,42 @@ public class NetClientGui extends JFrame
 			});
 		}
 	};
+	
+	private void showWeatherParameters(String weatherJson)
+	{
+		JSONObject json = new JSONObject(weatherJson);
+		
+		try
+		{
+			// Get date from dt - time in ms since January 1st, 1970 GMT
+			double dt = json.getDouble("dt");
+			Date weatherDate = new Date((long) dt * 1000);
+			
+			JSONObject main = json.getJSONObject("main");
+			double temp = main.getDouble("temp");
+			double pressure = main.getDouble("pressure");
+			double humidity = main.getDouble("humidity");
+			
+			JSONObject wind = json.getJSONObject("wind");
+			double windSpeed = wind.getDouble("speed");
+			double windDir = wind.getDouble("deg");
+			weatherInfoLabel.setText(String.format(weatherFormat, weatherDate.toString(), temp, pressure, 
+												   				  humidity, windSpeed, windDir));
+		}
+		catch (JSONException je)
+		{
+			//System.out.println(weatherJson);
+			weatherInfoLabel.setText("At least one of the weather parameters is missing.");
+		}
+	}
+	
+	private void showCurrencyRates()
+	{
+		double countryToForeignCurrRate = netService.getRateFor(foreignCurrencyTextField.getText());
+		double plnToForeignCurrRate = netService.getNBPRate();
+		currencyInfoLabel.setText(String.format(currencyFormat, netService.getCountryCurrency().toUpperCase(), 
+																netService.getCurrencyToCompareWith().toUpperCase(), 
+																countryToForeignCurrRate, 
+																plnToForeignCurrRate));
+	}
 }
--- TPO/TPO2_PP_S13616/src/zad1/Service.java
+++ TPO/TPO2_PP_S13616/src/zad1/Service.java
@@ -17,27 +17,33 @@ import org.json.*;
 public class Service 
 {
 	private String country;
-	private Locale countryLocale = null;
+	private Locale countryLocale;
 	private String countryCurrency;
+	private String currencyToCompareWith;
 	private static final String API_FIXER_BASE = "https://api.fixer.io";
 	private static final String OPEN_WEATHER_BASE = "https://api.openweathermap.org";
 	private static final String OPEN_WEATHER_APPID = "1f49695d4a9aed2b6c93a8ba1552e935";
+	private static final String API_NBP_BASE = "http://api.nbp.pl";
 	
 	public Service(String country)
 	{
 		this.country = country;
+		Locale.setDefault(new Locale("US"));
+		currencyToCompareWith = "PLN";	// Default value, just in case
 		findCountryInLocale();
 	}
 	
 	private void findCountryInLocale()
 	{
 		// TODO: find more efficient solution of Country --> Locale for country problem
+		//		 Maybe Map<String, Locale> to find locale easier.
 		Locale []availableLocales = Locale.getAvailableLocales();
 		for (Locale l : availableLocales)
 		{
+			//System.out.println(l.getDisplayName());
 			if (l.getDisplayCountry().equals(country))
 			{
-				Locale countryLocale = l;
+				countryLocale = l;
 				countryCurrency = Currency.getInstance(countryLocale).getCurrencyCode();
 				return;
 			}
@@ -52,13 +58,101 @@ public class Service
 		return getResponse(url);
 	}
 	
+	/*
+	 * Sample Fixer.io response:
+	 * {
+	 * 	"base":"PLN",
+	 * 	"date":"2018-03-28",
+	 * 	"rates":
+	 * 		{
+	 * 			"AUD":0.38344,
+	 * 			"BGN":0.46487,
+	 * 			"BRL":0.98079,
+	 * 				  ...
+	 * 			"USD":0.29469,
+	 * 			"ZAR":3.4472
+	 * 		}
+	 * }
+	*/
 	// Nice example using Gson: http://www.javatraineronline.com/java/simple-currency-converter-using-java/
 	public Double getRateFor(String currency)
 	{
+		currencyToCompareWith = currency;
 		String url = API_FIXER_BASE + "/latest?base=" + countryCurrency;
 		String response = getResponse(url);
-		//System.out.println(response);
-		return getCurrencyRate(currency, response);
+		return getCurrencyRateFromResponse(currencyToCompareWith, response);
+	}
+	
+	// http://www.oracle.com/technetwork/articles/java/json-1973242.html
+	private double getCurrencyRateFromResponse(String forCurrency, String serviceResponse)
+	{
+		JSONObject json = new JSONObject(serviceResponse);
+		double rate = 0.0; 
+		
+		try
+		{
+			rate = json.getJSONObject("rates").getDouble(forCurrency);
+		}
+		catch (JSONException je)
+		{
+			// Error while parsing JSON
+		}
+		catch (Exception e)
+		{
+			// Unknown exception
+		}
+		
+		return rate;
+	}
+	
+	/*
+	 * Sample response from NBP:
+	 * {
+	 * 	"table":"A",
+	 * 	"currency":"frank szwajcarski",
+	 * 	"code":"CHF",
+	 * 	"rates":
+	 * 		[{
+	 * 			"no":"062/A/NBP/2018",
+	 * 			"effectiveDate":"2018-03-28",
+	 * 			"mid":3.5731
+	 * 		}]
+	 * 	} 
+	 * 
+	*/
+	public Double getNBPRate()
+	{
+		if (countryCurrency.equals("PLN"))
+			return 1.0;		// PLN ==> PLN rate = 1.0
+
+		double rate = 0.0;
+		String url = API_NBP_BASE + "/api/exchangerates/rates/a/" + countryCurrency + "/";
+		String response = getResponse(url);
+		
+		if (response.isEmpty())
+		{
+			// try to find in a b table
+			url = API_NBP_BASE + "/api/exchangerates/rates/b/" + countryCurrency + "/";
+			response = getResponse(url);
+			if (response.isEmpty())
+			{
+				// TODO: Include some kind of notification?!
+				return rate;
+			}
+		}
+		
+		try
+		{
+			JSONObject json = new JSONObject(response);
+			JSONObject jsonRates = (JSONObject) json.getJSONArray("rates").get(0);
+			rate = jsonRates.getDouble("mid");
+		}
+		catch (Exception e)
+		{
+			// TODO: Catch JSONExceptions instead of the generic Exception
+		}
+
+		return rate;
 	}
 	
 	private String getResponse(String strUrl)
@@ -86,40 +180,20 @@ public class Service
 		}
 		catch (Exception e)
 		{
-			// ERROR!
 			// TODO: Handle MalformedURLException and IOException!
-			System.out.println(e.toString());
+			//System.out.println(e.toString());
 		}
 		
 		return sb.toString();
 	}
 	
-	// http://www.oracle.com/technetwork/articles/java/json-1973242.html
-	private double getCurrencyRate(String forCurrency, String serviceResponse)
+	public String getCountryCurrency()
 	{
-		JSONObject json = new JSONObject(serviceResponse);
-		double rate = 0.0; 
-		try
-		{
-			rate = json.getJSONObject("rates").getDouble(forCurrency);
-		
-		}
-		catch (JSONException je)
-		{
-			// Error while parsing JSON
-		}
-		catch (Exception e)
-		{
-			// Unknown exception
-		}
-		
-		return rate;
+		return countryCurrency;
 	}
 	
-	public Double getNBPRate()
+	public String getCurrencyToCompareWith()
 	{
-		// TODO: Implemntation!
-		return new Double(0.0);
+		return currencyToCompareWith;
 	}
-	
 }  
